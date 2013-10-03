2995f36423dcdb0ef738d078a7ccf704ffd85430
joshuaskling <joshuaskling@gmail.com>
2013-10-02 23:47:57 -0700

    added search bar

---
+++ script.js
@@ -0,0 +1,22 @@
+var searchField = $('input[type=search]'),
+    userInput;
+
+searchField.keyup(function() {
+  userInput = $(this).val();
+  $(this).val(userInput.replace(/[^0-9\.]/g,''));
+  userInput = $(this).val();
+  jumpToSection(userInput);
+});
+
+function jumpToSection(statusCode) {
+  var section = $('#'+statusCode);
+  if(section.length) {    
+    $('html, body').animate({
+      scrollTop: section.offset().top
+    }, 0);
+  }
+}
+
+$(function() {
+    searchField.focus();
+});
